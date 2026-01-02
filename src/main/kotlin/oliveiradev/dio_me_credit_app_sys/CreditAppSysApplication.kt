c893978905a5601a202322e90e15ac696880710d
Rodrigo Oliveira <r.oliveira.95@hotmail.com>
2026-01-02 14:28:09 -0300

    first commit

---
+++ src/main/kotlin/oliveiradev/dio_me_credit_app_sys/CreditAppSysApplication.kt
@@ -0,0 +1,11 @@
+package oliveiradev.dio_me_credit_app_sys
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditAppSysApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditAppSysApplication>(*args)
+}
---
+++ src/test/kotlin/oliveiradev/dio_me_credit_app_sys/CreditAppSysApplicationTests.kt
@@ -0,0 +1,13 @@
+package oliveiradev.dio_me_credit_app_sys
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditAppSysApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
